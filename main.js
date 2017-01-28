f17e4d3bdf1a336c90d9c157a12ee1035f447239
lucero-develops <mike.j.lucero@gmail.com>
2017-01-28 12:35:53 -0500

    starter code

---
+++ main.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Ice Cream (3-6)
+ */
+
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "strawberry";
+var vessel = "cone";
+var toppings = "cookies";
+
+// your code goes here
